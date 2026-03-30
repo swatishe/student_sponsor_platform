707ccf16ce2450d8fee1aaca5dccf1bd13895946
Swati Shende <swatishende@Swatis-MacBook-Air.local>
2026-03-30 03:47:55 -0400

    Allowed hosts fix

--- backend/ssp_project/settings.py
+++ backend/ssp_project/settings.py
@@ -12,8 +12,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY   = config('SECRET_KEY')
 DEBUG        = config('DEBUG', default=False, cast=bool)
-#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = [
+    '.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
